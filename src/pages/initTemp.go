4df1fefa8a45a967bc2b3fefcabb692787b5c211
KLOTZ alexandre <klotzalexandre431@gmail.com>
2025-11-28 11:07:21 +0100

    ajout de route et gomod

--- src/pages/initTemp.go
+++ src/pages/initTemp.go
@@ -1,1 +1,47 @@
 package pages
+
+import (
+	"html/template"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+var Temp *template.Template
+
+// Fonction custom utilisable dans les templates
+func add1(x int) int {
+	return x + 1
+}
+
+func Init() {
+	Temp = template.New("").Funcs(template.FuncMap{
+		"add1": add1,
+	})
+
+	basePath, err := os.Getwd()
+	if err != nil {
+		log.Fatalf("Erreur récupération chemin actuel : %v", err)
+	}
+
+	templatesPath := filepath.Join(basePath, "pages") // <-- juste "pages"
+
+	err = filepath.Walk(templatesPath, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() && filepath.Ext(path) == ".html" {
+			_, e := Temp.ParseFiles(path)
+			if e != nil {
+				log.Printf("Erreur parse template %s : %v", path, e)
+			}
+		}
+		return nil
+	})
+
+	if err != nil {
+		log.Fatalf("Erreur lors de l'initialisation des templates : %v", err)
+	}
+
+	log.Println("Templates chargés depuis :", templatesPath)
+}
